e930c6b59863d266a300656f3e01fe8f8974807f
Niccolò <Torresan>
2019-02-02 17:43:34 +0100

    Semaforo Austriaco con ultime modifiche completato

--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -1,11 +1,18 @@
-
-int rosso1 = 12;
+//assegno alle porte della scheda di arduino i vari collegati
+int rosso1 = 8;  
 int giallo1 = 10;
-int verde1 = 8;
+int verde1 = 12;
 
-int rosso2 = 6;
+int rosso2 = 2;
 int giallo2 = 4;
-int verde2 = 2;
+int verde2 = 6;
+
+//dichiaro le varibili riguardanti tempo e i lampeggi dei led
+int numLampeggi = 0;
+int tempoLamp = 0;
+int tempoVerde = 0;
+int tempoGiallo = 0;
+int tempoRosso = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,49 +23,83 @@ void setup() {
   pinMode(rosso2, OUTPUT);
   pinMode(giallo2, OUTPUT);
   pinMode(verde2, OUTPUT);
+  //Comandi che permettono all'utente di scegliere i tempi e
+  //e i lampeggi dei vari tempi
+  Serial.begin(9600);
+  
+  Serial.println ("Quanti lampeggi deve fare il verde?");
+  while (Serial.available() == 0) {};
+  numLampeggi = Serial.readString().toInt();
+
+  Serial.println ("Quanto durano i lampeggi?");
+  while (Serial.available() == 0) {};
+  tempoLamp = Serial.readString().toInt();
+
+  Serial.println ("Quanto sta acceso il verde?");
+  while (Serial.available() == 0) {};
+  tempoVerde = Serial.readString().toInt();
+
+  Serial.println ("Quanto sta acceso il giallo?");
+  while (Serial.available() == 0);
+  tempoGiallo = Serial.readString().toInt();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  tempoRosso = tempoVerde + tempoGiallo;
+  
   digitalWrite(rosso1, HIGH);
   digitalWrite(verde2, HIGH);
-  delay(5000);
-  
-  lampeggia(verde2, 500, 4);
+  delay(tempoVerde);
+
+  lampeggia2();
   digitalWrite(verde2, LOW);
 
   digitalWrite(giallo1, HIGH);
   digitalWrite(giallo2, HIGH);
-  digitalWrite(rosso1, LOW);
-  delay(3000);
+  delay(tempoGiallo);
 
   digitalWrite(rosso1, LOW);
   digitalWrite(giallo1, LOW);
   digitalWrite(giallo2, LOW);
   digitalWrite(verde1, HIGH);
   digitalWrite(rosso2, HIGH);
-  delay(5000);
+  delay(tempoVerde);
 
-  lampeggia(verde1, 500, 4);
+  lampeggia1();
   digitalWrite(verde1, LOW);
 
   digitalWrite(giallo1, HIGH);
   digitalWrite(giallo2, HIGH);
-  digitalWrite(rosso2, LOW);
-  delay(3000);
-
+  delay(tempoGiallo);
+  
   digitalWrite(giallo2, LOW);
   digitalWrite(giallo1, LOW);
-  delay(5000);
-}
-
- void lampeggia(int led, int ritardo, int n)
- {
-  for(int i = 0; i <= n; i++)
-  {
-    delay(ritardo);
-    digitalWrite(led, LOW);
-    delay(ritardo);
-    digitalWrite(led, HIGH);
+  digitalWrite(rosso2, LOW);
   }
- }
+
+  //metodi che permettono di far lampeggiare i due led verdi
+  void lampeggia1()
+  {
+   for(int i = 0; i <= numLampeggi; i++)
+   {
+    delay(tempoLamp);
+    digitalWrite(verde1, LOW);
+    delay(tempoLamp);
+    digitalWrite(verde1, HIGH);
+   }
+  }
+
+  void lampeggia2()
+  {
+   for(int i = 0; i <= numLampeggi; i++)
+   {
+    delay(tempoLamp);
+    digitalWrite(verde2, LOW);
+    delay(tempoLamp);
+    digitalWrite(verde2, HIGH);
+   }
+  }
+ 
+ 
